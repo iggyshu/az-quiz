0d321b50d52ccf3d20a7e74dddb94c32a891638d
Igors Šutovs <iggy.shu@mykolab.com>
2023-06-16 16:48:59 +0200

    implement color changes

--- hexagon.gd
+++ hexagon.gd
@@ -1,5 +1,11 @@
 extends Area2D
 
+var is_checked = false
+var is_black = false
+var is_selected = false
+
+signal selected
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -7,7 +13,26 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if is_selected:
+		$TileColor.play("dark_yellow")
 
 func set_value(value):
 	$NumberLabel.text = value
+
+func set_color(color):
+	$TileColor.play(color)
+
+
+func _on_mouse_entered():
+	if not is_selected:
+		$TileColor.play("yellow")
+
+func _on_mouse_exited():
+	if not is_selected:
+		$TileColor.play("empty")
+
+
+func _on_input_event(viewport, event, shape_idx):
+	if (not is_selected and event is InputEventMouseButton and event.pressed):
+		is_selected = true
+		selected.emit()
