ab317e921075d56a680eba1a8965b49306804f98
Igors Šutovs <iggy.shu@mykolab.com>
2023-08-01 14:06:55 +0200

    add pathfinding tests

--- test/test_graph.gd
+++ test/test_graph.gd
@@ -32,3 +32,58 @@ func test_claim_node_already_claimed_node():
 
 	# Then the node is not claimed
 	assert_false(result, "Should not be able to claim an already claimed node")
+
+func test_check_path_exists_path_does_not_exist():
+	# Given a graph and no path connecting three triangle sides
+	var graph = Graph.new()
+	graph.claim_node(1, "BLUE")
+	graph.claim_node(7, "RED")
+	graph.claim_node(21, "BLUE")
+
+	# When I check for complete paths
+	var result = graph.check_path_exists("BLUE")
+
+	# Then there are no complete paths
+	assert_false(result, "Should not have any complete paths")
+
+func test_check_path_exists_path_does_exist():
+	# Given a graph and a path connecting three triangle sides
+	var graph = Graph.new()
+	graph.claim_node(1, "BLUE")
+	graph.claim_node(2, "BLUE")
+	graph.claim_node(4, "BLUE")
+	graph.claim_node(7, "BLUE")
+	graph.claim_node(11, "BLUE")
+	graph.claim_node(16, "BLUE")
+	graph.claim_node(22, "BLUE")
+	graph.claim_node(3, "RED")
+	graph.claim_node(6, "RED")
+
+	# When I check for complete paths
+	var result = graph.check_path_exists("BLUE")
+
+	# Then there is a complete path (1, 2, 4, 7, 11, 16, 22)
+	assert_true(result, "Should have a complete path")
+
+func test_check_path_exists_complex_path_does_exist():
+	# Given a graph and a complex path connecting three triangle sides
+	var graph = Graph.new()
+	graph.claim_node(1, "RED")
+	graph.claim_node(3, "RED")
+	graph.claim_node(6, "RED")
+	graph.claim_node(9, "RED")
+	graph.claim_node(13, "RED")
+	graph.claim_node(12, "RED")
+	graph.claim_node(17, "RED")
+	graph.claim_node(24, "RED")
+	graph.claim_node(5, "BLUE")
+	graph.claim_node(4, "BLUE")
+	graph.claim_node(8, "BLUE")
+	graph.claim_node(18, "BLUE")
+	graph.claim_node(11, "BLUE")
+
+	# When I check for complete paths
+	var result = graph.check_path_exists("RED")
+
+	# Then there is a complete path (1, 3, 6, 9, 13, 12, 17, 24)
+	assert_true(result, "Should have a complete path")
